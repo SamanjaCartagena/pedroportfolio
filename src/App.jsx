b3f9efb742a962a36150d775d65341d714e72875
Samanja Cartagena <c.samanja09@Pabs.local>
2025-05-04 03:37:03 -0400

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
-
+import {useState} from 'react'
+import { LoadingScreen } from './components/LoadingScreen'
+import './index.css'
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
 
   return (
     <>
-      <div>
-      
-      </div>
+      {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)}/>}{""}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100":"opacity-0"
+        
+        } bg-black text-gray-100
+        `}>
+          <Navbar />
+
+        </div>
     
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,3 @@
+export const Navbar =() => {
+    return <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,.8)] "></nav>
+}
